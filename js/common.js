2299324dd6dee592f0cef06b3c4421f42bfd5537
bigbuks <badmanbuks7@gmail.com>
2025-03-10 22:18:16 +0100

    Second commit

--- js/common.js
+++ js/common.js
@@ -37,3 +37,12 @@ function toggleText() {
       btn.innerHTML = "View More";
   }
 }
+
+
+function openModal(img) {
+    document.getElementById("modalImage").src = img.src;
+    document.getElementById("imageModal").style.display = "flex";
+}
+function closeModal() {
+    document.getElementById("imageModal").style.display = "none";
+}
